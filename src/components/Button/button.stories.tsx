bc87fbdd9477bfcab3f5a0cee8744b7263f8a862
Nurbolat Kenzhekulov <nurbolattk@gmail.com>
2022-10-28 01:12:26 +0600

    install storybook & button stories

---
+++ src/components/Button/button.stories.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import type {Meta, StoryObj} from '@storybook/react'
+
+import {Button} from './index'
+
+export default {
+  title: 'Button',
+  component: Button
+} as Meta<typeof Button>
+
+export const Default: StoryObj<typeof Button> = {
+  render: () => <Button>Button Label</Button>
+}
+
+export const Primary: StoryObj<typeof Button> = {
+  render: () => <Button color="primary">Button Label</Button>
+}
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,15 +1,19 @@
-import { ButtonHTMLAttributes, HTMLAttributes } from 'react'
+import { ButtonHTMLAttributes } from 'react'
 import cx from 'clsx'
 import classes from './button.module.css'
 
-type COLOR = 'default' | 'primary' | 'danger'
-type VARIANT = 'filled' | 'outline' | 'text'
-type size = 'medium' | 'small'
+type Color = 'default' | 'primary' | 'danger'
+type Variant = 'filled' | 'outline' | 'text'
+type Size = 'md' | 'sm'
 
 export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
-  isPrimary?: boolean
+  color?: Color
+  variant?: Variant
+  size?: Size
 }
 
-export const Button = ({ className, isPrimary = false, ...props }: ButtonProps) => {
-  return <button className={cx(className, classes.button)} {...props} />
+export const Button = ({ className, color = 'default', variant = 'filled', size = 'md', ...props }: ButtonProps) => {
+  return <button className={cx(className, classes.button, {
+    [classes.primary]: color === 'primary'
+  })} {...props} />
 }
